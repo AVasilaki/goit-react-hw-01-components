0fd53a9157376da7226ac7f82b4f7e1d31255b34
AVasilaki <poseydon463@gmail.com>
2023-10-01 18:41:02 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,22 @@
 import user from '../user.json';
 import { Profile } from './Profile';
-import data from '../data.json'
+import data from '../data.json';
 import { Panel } from './Statistics';
 import { Statistics } from './Statistics';
-import friends from '../friends.json'
+import friends from '../friends.json';
 import { FriendList } from './Frands';
 export const App = () => {
   return (
     <div>
-     <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
       />
       <Panel title="Upload stats">
-      <Statistics  stats={data} />
+        <Statistics stats={data} />
       </Panel>
       <FriendList friends={friends} />;
     </div>
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,18 +1,21 @@
 export const Panel = ({ title, children }) => {
-    return <section className="statistics">
-    <h2 className="title">{title }</h2>
-    {children}
-  </section>
-}
+  return (
+    <section className="statistics">
+      <h2 className="title">{title}</h2>
+      {children}
+    </section>
+  );
+};
 
 export const Statistics = ({ stats }) => {
-   
-    return <ul className="stat-list">
-            {stats.map(data => (
-                <li key={data.id} className="item">
-                    <span className="label">{ data.label }</span>
-                 <span className="percentage">{data.percentage} </span>
-                </li>))}
-          </ul>
-
-}
+  return (
+    <ul className="stat-list">
+      {stats.map(data => (
+        <li key={data.id} className="item">
+          <span className="label">{data.label}</span>
+          <span className="percentage">{data.percentage} </span>
+        </li>
+      ))}
+    </ul>
+  );
+};
